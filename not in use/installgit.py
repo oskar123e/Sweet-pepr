8ab8a2d476a3a045f6d820c1ea9926445c2472a0
oskar123e <oskarz17@yahoo.com>
2016-10-29 11:48:59 +0100

    my ides and stuff

---
+++ not in use/installgit.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('sudo ' + 'apt-get' + 'install' + ' git ' + 'scala ' + 'eclipse ' + 'qbittorrent p')
+os.system('sudo add-apt-repository ppa:mystic-mirage/pycharm')
+os.system('sudo apt-get update')
+os.system('sudo apt-get install pycharm')
