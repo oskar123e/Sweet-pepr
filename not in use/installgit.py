5cd5bd01e66942b605c18dfb0f85340a064c4987
oskar123e <oskarz17@yahoo.com>
2016-10-29 22:15:59 +0100

    version 0.1

--- not in use/installgit.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system('sudo ' + 'apt-get' + 'install' + ' git ' + 'scala ' + 'eclipse ' + 'qbittorrent p')
-os.system('sudo add-apt-repository ppa:mystic-mirage/pycharm')
-os.system('sudo apt-get update')
-os.system('sudo apt-get install pycharm')
